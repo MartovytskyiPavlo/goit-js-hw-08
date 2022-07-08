fcd72c56e30131f6404ed8339d3f62dca568910d
Martovytskyi Pavlo <viy.1979@gmail.com>
2022-07-08 12:31:26 +0300

    gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -4,6 +4,7 @@ import { galleryItems } from './gallery-items';
 
 // Описаний в документації
 import SimpleLightbox from "simplelightbox";
+
 // Додатковий імпорт стилів
 import "simplelightbox/dist/simple-lightbox.min.css";
 
