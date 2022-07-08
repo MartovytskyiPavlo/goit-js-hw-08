39ea7396756d9150f839dc4974c506f3bff9f5db
Martovytskyi Pavlo <viy.1979@gmail.com>
2022-07-08 11:54:50 +0300

    gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -7,4 +7,29 @@ import SimpleLightbox from "simplelightbox";
 // Додатковий імпорт стилів
 import "simplelightbox/dist/simple-lightbox.min.css";
 
+const galleryContainer = document.querySelector('.gallery');
+
+const listItemsMarkup = createListItemsMarkup(galleryItems);
+
+galleryContainer.innerHTML = listItemsMarkup;
+
+function createListItemsMarkup(gallery){
+    const markup = gallery.map(({preview, original, description}) => {
+        return `<a class="gallery__item" href="${original}">
+                    <img class="gallery__image" src="${preview}" alt="${description}" />
+                </a>
+                `;
+    }).join('');
+    return markup;
+}
+
+let gallery = new SimpleLightbox('.gallery a', { 'captionsData' : 'alt', 'captionDelay': 250 });
+gallery.on('show.simplelightbox', function () {
+	// do somethingвЂ¦
+});
+
+gallery.on('error.simplelightbox', function (e) {
+	console.log(e); // some usefull information
+});
+
 console.log(galleryItems);
