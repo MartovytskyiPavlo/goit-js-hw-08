4855709cbdc7b74c928da7370eb3989a6aa9d874
Martovytskyi Pavlo <viy.1979@gmail.com>
2022-07-27 15:54:15 +0300

    video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,22 +1,12 @@
-import Player from '@vimeo/player';
+// import Player from '@vimeo/player';
 
 const iframe = document.querySelector('#vimeo-player');
 
 onReloadPage();
 
-
 const player = new Vimeo.Player(iframe);
 
-// player.on('play', function() {
-//     console.log('played the video!');
-// });
-
-// player.getVideoTitle().then(function(title) {
-//     console.log('title:', title);
-// });
-
 localStorage.setItem("play", JSON.stringify(seconds));
-
 
 function onReloadPage() {
     const timeline = localStorage.getItem("timeline");
