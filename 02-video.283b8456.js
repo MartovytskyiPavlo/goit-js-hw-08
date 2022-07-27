5a3bf713226f962dcec293d4d4ac0b7ff6f32982
MartovytskyiPavlo <101937766+MartovytskyiPavlo@users.noreply.github.com>
2022-07-27 12:54:49 +0000

    Deploying to gh-pages from @ MartovytskyiPavlo/goit-js-hw-08@4855709cbdc7b74c928da7370eb3989a6aa9d874 🚀

---
+++ 02-video.283b8456.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("#vimeo-player");localStorage.getItem("timeline")?console.log("Time loaded!"):console.log("First visit.");new Vimeo.Player(e);localStorage.setItem("play",JSON.stringify(seconds))}();
+//# sourceMappingURL=02-video.283b8456.js.map
