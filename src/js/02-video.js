2a71846d6521891139cff20b538aa8c517cd08c5
Martovytskyi Pavlo <viy.1979@gmail.com>
2022-07-27 13:37:59 +0300

    video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,16 +2,6 @@ import Player from '@vimeo/player';
 
 const iframe = document.querySelector('#vimeo-player');
 
-const options = {
-    id: 236203659,
-    width: 640,
-    height:360,
-    frameborder:0,
-    allowfullscreen,
-    allow:"autoplay; encrypted-media",
-    loop: true
-    };
-
 const player = new Player(iframe,options);
 
 player.on('play', function() {
