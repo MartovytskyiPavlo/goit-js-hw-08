b813e3e247cee743ecff1ccbff8aace1bbaa23c2
Martovytskyi Pavlo <viy.1979@gmail.com>
2022-07-28 15:10:00 +0300

    form

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,17 +1,17 @@
+import throttle from 'lodash.throttle';
+
 const STORAGE_KEY = "feedback-form-state";
 
 const storageObject = {};
 
 const form = document.querySelector(".feedback-form");
 
-onReloadPage();
-
-form.addEventListener('input', onTextareaInput);
+form.addEventListener('input', throttle(onTextareaInput,500));
 form.addEventListener('submit', onFormSubmit);
 
-function onTextareaInput(evt) {
-    const message = evt.target.value;
+onReloadPage();
 
+function onTextareaInput(evt) {
     storageObject[evt.target.name] = evt.target.value;
 
     localStorage.setItem(STORAGE_KEY, JSON.stringify(storageObject));
@@ -23,19 +23,21 @@ function onReloadPage() {
     if (storage) {
         const obj = JSON.parse(storage);
 
-        for (let member in obj) {
-            form[member].value = obj[member];
+        for (const name in obj) {
+            form[name].value = obj[name];
         }
     }
 }
 
 function onFormSubmit(evt) {
     evt.preventDefault();
+    evt.currentTarget.reset();
 
     console.log(storageObject);
 
-    for (let member in storageObject) {
-        delete storageObject[member];
-        form[member].value = "";
+    for (const name in storageObject) {
+        delete storageObject[name];
     }
+
+    localStorage.removeItem(STORAGE_KEY);
 }
